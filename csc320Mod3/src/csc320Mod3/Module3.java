734bed06b3894549e83230cec25f2e55e0b831e0
matt-finney <matthew.finney@hotmail.com>
2025-08-21 12:43:11 -0400

    added Module 3 for next week

---
+++ csc320Mod3/src/csc320Mod3/Module3.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package csc320Mod3;
+
+/**
+ * 
+ */
+public class Module3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
