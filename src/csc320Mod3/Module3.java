977bdce30896bfd1e589dd86f97d37cba732210e
matt-finney <matthew.finney@hotmail.com>
2025-09-05 10:47:39 -0400

    helper methods, calcs weeks 1-4, and avg weekly income

--- src/csc320Mod3/Module3.java
+++ src/csc320Mod3/Module3.java
@@ -2,51 +2,96 @@ package csc320Mod3;
 
 import java.util.Scanner;
 
-// Matthew Finney
-// A program that will calculate the weekly tax withholding for 
-// a customer given weekly income guidelines.
+/* 
+ * Matthew Finney
+ * Collects income for weeks 1-4 and sums their witholding amounts to display
+ * as well as average weekly withholding
+ * https://github.com/matt-finney/CSC320
+ */ 
 
-public class Module3 {
-	public static void main(String[] args) {
-		Scanner scnr = new Scanner(System.in);
+public class Module3 
+{
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
 		
 		// variables
-		double income;
+		final int NUM_WEEKS = 4;
+		double income = 0;
 		double tax_percentage;
 		double tax_witholding;
+		double avg_week_withholding;
 		
-		// get user input
-		System.out.print("Enter weekly income: ");
-		income = scnr.nextInt();
-		
-		
-		// validate input to prevent negative numbers
-		if (income < 0) 
+		// get income for weeks 1-4
+		for (int i = 1; i <= NUM_WEEKS; ++i) 
 		{
-			System.out.println("Error: Income cannot be negative.");
-			scnr.close();
-			return;
+			income += getIncomeForWeek(input, i);
 		}
 		
-		// determine tax percentage based on income brackets
-		if (income < 500) 
-			tax_percentage = 0.10;
-		else if (income >= 500 && income < 1500) 
-			tax_percentage = 0.15;
-		else if (income >= 1500 && income < 2500) 
-			tax_percentage = 0.20;
-		else
-			tax_percentage = 0.25;
+		// get tax percentage based on income brackets
+		tax_percentage = getTaxPercentage(income);
 		
-		// calculate tax withholding
-		tax_witholding = income * tax_percentage;
+		// get tax withholding
+		tax_witholding = getTaxWithholding(income, tax_percentage);
+
+		// get average weekly withholding
+		avg_week_withholding = getAverageIncome(income, NUM_WEEKS);
 		
 		// display results
-		System.out.printf("Weekly Income: $%.2f\n", income);
-		System.out.printf("Tax Percentage: %.0f%%\n", tax_percentage * 100);
-		System.out.printf("Tax Withholding: $%.2f\n", tax_witholding);
-		
-		scnr.close();
+		printResults(income, tax_percentage, tax_witholding, avg_week_withholding);
+
+		input.close();
+	}
+	
+	// get the income for a given week
+	// validate input to prevent negative numbers
+	public static double getIncomeForWeek(Scanner input, int week_num) 
+	{
+		double income;
+		while (true) 
+		{
+			System.out.printf("Enter income for week %d: ", week_num);
+			income = input.nextDouble();
+			if (income >= 0) 
+			{
+				return income;
+			} 
+			else 
+			{
+				System.out.println("Income cannot be negative. Please try again.");
+			}
+		}
 	}
 
+	// calculate tax withholding based on income and tax percentage
+	public static double getTaxWithholding(double income, double tax_percentage) 
+	{
+		return income * tax_percentage;
+	}
+
+	public static double getAverageIncome(double income, int num_weeks)
+	{
+		return income / num_weeks; 
+	}
+
+	// determine tax percentage based on income brackets
+	public static double getTaxPercentage (double income) 
+	{
+		if (income < 500) 
+			return 0.10;
+		else if (income >= 500 && income < 1500) 
+			return 0.15;
+		else if (income >= 1500 && income < 2500) 
+			return 0.20;
+		else
+			return 0.30;
+	}
+
+	public static void printResults(double income, double tax_percentage, double tax_withholding, double avg_weekly_withholding) 
+	{
+		System.out.printf("\nMonthly Income: $%.2f\n", income);
+		System.out.printf("Month's Tax Percentage: %.0f%%\n", tax_percentage * 100);
+		System.out.printf("Month's Tax Withholding: $%.2f\n", tax_withholding);
+		System.out.printf("Average Weekly Withholding: $%.2f\n", avg_weekly_withholding);
+	}
 }
