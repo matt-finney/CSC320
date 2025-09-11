535efefb37ba12161085deec977ca777d0abf882
Matt <matthew.finney@hotmail.com>
2025-09-11 13:33:18 -0400

    added a test file for discussion examples

--- src/testing/Test.java
+++ src/testing/Test.java
@@ -3,18 +3,20 @@ package testing;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
-        clearScreen();
+    public static void main(String[] args) 
+    {
         Scanner scanner = new Scanner(System.in);
         String[] shoppingCartIDs = new String[5];
         // Loop to listen for items to add to cart
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) 
+        {
             System.out.print("Enter item ID to add to cart: ");
             shoppingCartIDs[i] = scanner.nextLine();
         }
         System.out.println();
         // Example IDs: "X0010234", "X0033455", "X0067853", "X0098765", "X0012345"
-        for (String id : shoppingCartIDs) {
+        for (String id : shoppingCartIDs) 
+        {
             System.out.println("Shopping Cart ID: " + id);
         }
         scanner.close();
