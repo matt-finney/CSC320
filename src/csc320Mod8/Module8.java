197efe992a4566356b07a4148af918d97a001e7a
Matt <matthew.finney@hotmail.com>
2025-09-29 20:52:52 -0400

    Add Module8 class

---
+++ src/csc320Mod8/Module8.java
@@ -0,0 +1,16 @@
+package csc320Mod8;
+
+public class Module8 
+{
+    public static void main(String[] args) 
+    {
+        try 
+        {
+          Automobile car1 = new Automobile("Honda", "Civic", "Red", 2018);
+          System.out.println(car1.displayVehicleInfo());
+        } catch (Exception e) 
+        {
+          System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
