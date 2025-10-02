a4e4e8574407c4336864394731e1a31c3ad17a9b
Matt <matthew.finney@hotmail.com>
2025-10-02 11:27:37 -0400

    Added steps 1-3

--- src/csc320Mod8/Module8.java
+++ src/csc320Mod8/Module8.java
@@ -4,13 +4,35 @@ public class Module8
 {
     public static void main(String[] args) 
     {
-        try 
-        {
-          Automobile car1 = new Automobile();
-          System.out.println(car1.displayVehicleInfo());
-        } catch (Exception e) 
-        {
-          System.out.println("An error occurred: " + e.getMessage());
-        }
+      // Step 1: Default Constructor
+      try 
+      {
+        // call default constructor
+        Automobile car1 = new Automobile();
+        System.out.println(car1.displayVehicleInfo());
+      } catch (Exception e) 
+      {
+        System.out.println("An error occurred: " + e.getMessage());
+      }
+      
+      // Step 2: Parametereized Constructor
+      Automobile car2 = new Automobile("Toyota", "Camry", "Blue", 2020, 15000);
+      // list info and loop through array
+      String[] info = car2.listVehicleInformation();
+      for (String line : info) 
+      {
+          System.out.println(line);
+      }
+
+      // Step 3: Remove Vehicle
+      // returns success or failure message
+      System.out.printf("\n%s\n\n", car2.removeVehicle());
+      
+      // show that the fields have been cleared
+      String[] clearedInfo = car2.listVehicleInformation();
+      for (String line : clearedInfo)
+      {
+        System.out.println(line);
+      }
     }
 }
