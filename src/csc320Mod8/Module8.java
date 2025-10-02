cf05bda6761a22a84c6894ba08023059aea204b0
Matt <matthew.finney@hotmail.com>
2025-10-02 14:32:04 -0400

    Added write to file, reformatted newline to %n

--- src/csc320Mod8/Module8.java
+++ src/csc320Mod8/Module8.java
@@ -1,5 +1,7 @@
 package csc320Mod8;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Module8 
@@ -11,8 +13,7 @@ public class Module8
       {
         // call default constructor
         Automobile car1 = new Automobile();
-        System.out.println("Step 1:");
-        System.out.println(car1.displayVehicleInfo());
+        System.out.printf("Step 1: %n%s%n%n", car1.displayVehicleInfo());
       } catch (Exception e) 
       {
         System.out.println("An error occurred: " + e.getMessage());
@@ -30,7 +31,7 @@ public class Module8
 
       // Step 3: Remove Vehicle
       // returns success or failure message
-      System.out.printf("\nStep 3:\n%s\n\n", car2.removeVehicle());
+      System.out.printf("%nStep 3:%n%s%n%n", car2.removeVehicle());
 
       // show that the fields have been cleared
       String[] clearedInfo = car2.listVehicleInformation();
@@ -41,27 +42,50 @@ public class Module8
       System.out.println();
 
       // Step 4: Add a new Vehicle
-      System.out.printf("\nStep 4:\n%s\n\n", car2.addVehicle("Honda", "Civic", "Black", 2024, 0));
+      System.out.printf("%nStep 4:%n%s%n%n", car2.addVehicle("Honda", "Civic", "Black", 2024, 0));
       System.out.println(car2.displayVehicleInfo());
 
       // Step 5: Update the Vehicle
-      System.out.printf("\nStep 5:\n%s\n\n", car2.updateVehicle(null, null, "White", -1, 15000));
+      System.out.printf("%nStep 5:%n%s%n%n", car2.updateVehicle(null, null, "White", -1, 15000));
       System.out.println(car2.displayVehicleInfo());
 
       // Step 6: Ask to save to file (Y/N)
       Scanner input = new Scanner(System.in);
+      // ask the user
       String response = getReponse(input, "Would you like to print the information to a file? (Y/N): ");
-      
-      
+      String file_path = "C:\\Temp\\Autos.txt";
+      // decide to write to file
+      if (response.equalsIgnoreCase("y"))
+      {
+        writeToFile(file_path, car2);
+      }
+      else
+      {
+        System.out.println("No file will be created.");
+      }
+    }
+    
+    // Helper method to write to file
+    public static void writeToFile(String file_path, Automobile car)
+    {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(file_path, true)))
+        {
+          writer.println(car.displayVehicleInfo());
+          writer.println();
+          System.out.printf("Vehicle information written to %s", file_path);
+        } catch (Exception e)
+        {
+          System.out.println("Failed to write file: " + e.getMessage());
+        }
     }
 
-    // Method to keep asking until the user enters 'y', 'n'
+    // Helper mhod to keep asking until the user enters 'y', 'n'
     public static String getReponse(Scanner scanner, String prompt)
     {
       while (true) 
       {
         System.out.print(prompt);
-        String input = scanner.nextLine();
+        String input = scanner.nextLine().trim();
 
         try 
         {
@@ -69,8 +93,12 @@ public class Module8
           {
             return input;
           }
+          else
+          {
+            throw new IllegalArgumentException("Invalid input");
+          }
         } 
-        catch (NumberFormatException e) 
+        catch (IllegalArgumentException e) 
         {
           System.out.println("Invalid input. Please enter y or n");
         }
