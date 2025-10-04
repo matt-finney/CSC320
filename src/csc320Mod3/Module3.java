dd7ea84d652fc26d981f0e7395d6fa76ae5a1ebb
Matt <matthew.finney@hotmail.com>
2025-10-04 11:47:27 -0400

    Update program description in comments

--- src/csc320Mod3/Module3.java
+++ src/csc320Mod3/Module3.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 
 /* 
  * Matthew Finney
- * Collects income for weeks 1-4 and sums their witholding amounts to display
- * as well as average weekly withholding
+ * Program that will calculate the weekly average tax withholding for a customer given the following weekly income guidelines:
+ * Income less than $500: tax rate 10%
+ * Incomes greater than/equal to $500 and less than $1500: tax rate 15%
+ * Incomes greater than/equal to $1500 and less than $2500: tax rate 20%
+ * Incomes greater than/equal to $2500: tax rate 30%
+ * 
  * https://github.com/matt-finney/CSC320
  */ 
 
